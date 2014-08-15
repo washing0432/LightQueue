586d51e3b8acfa6f9aa64dcba14a16bac8e958dc
washing <174649617@qq.com>
2014-08-15 10:57:51 +0800

    create pj

---
+++ QueueConsole/QueueManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace QueueConsole
+{
+    public class QueueManager
+    {
+        private static AutoResetEvent QueueTaskEvent = new AutoResetEvent(false);
+        private static AutoResetEvent workEvent = new AutoResetEvent(false);
+        private static Queue<IQueueTask> QueueTasks { get; set; }
+        private static Thread workThread = null;
+
+        static QueueManager()
+        {
+            QueueTasks = new Queue<IQueueTask>();
+            workThread = new Thread(DoingTask);
+            workThread.Start();
+        }
+
+        public static void Enqueue(IQueueTask pQueueTask)
+        {
+            QueueTasks.Enqueue(pQueueTask);
+            QueueTaskEvent.Set();
+            Thread.Sleep(200);
+        }
+
+        public static void Working()
+        {
+            while (true)
+            {
+                if (QueueTasks.Count == 0) continue;
+                workEvent.Set();
+                QueueTaskEvent.WaitOne();
+            }
+        }
+
+        public static void DoingTask()
+        {
+            while (true)
+            {
+                if (QueueTasks.Count == 0) return;
+                IQueueTask task = QueueTasks.Dequeue();
+                task.Do();
+
+                workEvent.WaitOne();
+            }
+        }
+    }
+}
---
+++ QueueConsole/QueueTask.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace QueueConsole
+{
+    public class QueueTask : IQueueTask
+    {
+        public void Do()
+        {
+            Console.WriteLine("{0} queue task done", Thread.CurrentThread.Name);
+        }
+    }
+}
